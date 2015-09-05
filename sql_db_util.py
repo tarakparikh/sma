e2491dec28372bad62669e08f0e0cd26a3482e53
tarakparikh <tmparikh@gmail.com>
2015-09-05 10:57:57 -0700

    Created read write routines

--- sql_db_util.py
+++ sql_db_util.py
@@ -12,33 +12,96 @@ import sqlite3
 
 #Print
 
-sqconn = sqlite3.connect("mystk.db");
-sqconn.execute('''CREATE TABLE STOCK_PRICES
+def create_tables():
+    sqconn = sqlite3.connect("mystk.db");
+    sqconn.execute('''CREATE TABLE STOCK_PRICES
        (ID INTEGER PRIMARY KEY     AUTOINCREMENT,
        SYMBOL           TEXT    NOT NULL,
        VAL        REAL    );''')
 
-sqconn.execute('''CREATE TABLE STOCK_SMA
+    sqconn.execute('''CREATE TABLE STOCK_SMA
        (ID INTEGER PRIMARY KEY     AUTOINCREMENT,
        SYMBOL           TEXT    NOT NULL,
        VAL      REAL    );''')
 
-sqconn.execute('''CREATE TABLE STOCK_SMA50
+    sqconn.execute('''CREATE TABLE STOCK_SMA50
        (ID INTEGER PRIMARY KEY     AUTOINCREMENT,
        SYMBOL           TEXT    NOT NULL,
        VAL      REAL    );''')
 
-sqconn.execute('''CREATE TABLE DATETABLE
+    sqconn.execute('''CREATE TABLE DATETABLE
        (
        ID INT PRIMARY KEY     NOT NULL,
        TODAY           TEXT    NOT NULL
        );''')
 
-sqconn.execute('''CREATE TABLE COMPANIES
+    sqconn.execute('''CREATE TABLE COMPANIES
        (ID INT PRIMARY KEY     NOT NULL,
 	NAME TEXT 		NOT NULL,
        SYMBOL           TEXT    NOT NULL);''')
 
+    sqconn.commit;
+
+def open_writer(dbname,data):
+    sqconn = sqlite3.connect("mystk.db");
+    today = date.today()
+    today_ordinal = date.toordinal(today)
+    sqconn.execute("UPDATE DATETABLE SET TODAY = ? WHERE ID=1", today_ordinal);
+    return writer
+
+#
+def write_history_symbol (dbname, data):
+    sqconn = sqlite3.connect("mystk.db");
+    symbol = data[0];
+    del data[0];
+    my_type_arr_2 = tuple([tuple([symbol,row]) for row in data]);
+    my_query_str = "INSERT INTO " + dbname + "(SYMBOL,VAL) VALUES (?, ?)"
+    sqconn.executemany( my_query_str, my_type_arr_2);
+    sqconn.commit()
+
+def write_daily_values (dbname, data):
+    sqconn = sqlite3.connect("mystk.db");
+    my_type_arr_2 = tuple([tuple(row) for row in data]);
+    print my_type_arr_2;
+    my_query_str = "INSERT INTO " + dbname + "(SYMBOL,VAL) VALUES (?, ?)"
+    sqconn.executemany( my_query_str, my_type_arr_2);
+    sqconn.commit()
+
+def open_reader(dbname):
+    stocks = [];
+    sqconn = sqlite3.connect("mystk.db");
+    cursor = sqconn.execute("SELECT DISTINCT symbol from STOCK_PRICES")
+    for row in cursor:
+	rowVal = [];
+	symbolName = row[0];
+	print symbolName;
+	rowVal.append(symbolName);
+	sel_query = "SELECT VAL from " + dbname + " where SYMBOL='" + symbolName + "'";
+	print sel_query;
+	symData = sqconn.execute(sel_query);
+	for val in symData:
+	    rowVal.append(val[0]);
+	stocks.append(rowVal);
+   
+    sqconn.close()
+    return stocks; 
+
+def check_update(dbname):
+    today = date.today()
+    dbFound = 0
+    today_ordinal = date.toordinal(today)
+    sqconn = sqlite3.connect("mystk.db");
+    cursor = sqconn.execute("SELECT TODAY from DateTable")
+    for row in cursor:
+	stored_date = row[0];
+    if (stored_date == today_ordinal):
+	dbFound = 1
+
+    sqconn.close()
+    return dbFound
+
+
+sqconn = sqlite3.connect("mystk.db");
 sqconn.execute("INSERT INTO STOCK_PRICES (SYMBOL,VAL) \
       VALUES ('MSFT', 100.0 )");
 
@@ -126,66 +189,37 @@ for row in cursor:
 #
 sqconn.close()
 
-def open_writer(dbname,data):
-    sqconn = sqlite3.connect("mystk.db");
-    today = date.today()
-    today_ordinal = date.toordinal(today)
-    sqconn.execute("UPDATE DATETABLE SET TODAY = ? WHERE ID=1", today_ordinal);
-    #fname = "db/stocks." + dbname + "." + "%s" % (today_ordinal) + ".csv"
-    #print fname
-    #writer = csv.writer(open(fname, 'wb', buffering=0))
-    return writer
+list1 = [
+	"MENT",
+	20.0,
+	25.0,
+	23.0,
+	24.0
+]
+write_history_symbol('STOCK_PRICES',list1);
+sqconn = sqlite3.connect("mystk.db");
+cursor = sqconn.execute("SELECT id, symbol, val  from STOCK_PRICES")
+for row in cursor:
+   print "ID = ", row[0]
+   print "SYMBOL = ", row[1]
+   print "VAL = ", row[2], "\n"
 
-#
-def write_history_symbol (dbname, data):
-    sqconn = sqlite3.connect("mystk.db");
-    symbol = data[0];
-    data.delete(0);
-    my_type_arr_2 = tuple([tuple([symbol,row]) for row in data]);
-    sqconn.executemany("INSERT INTO dbname (SYMBOL,VAL) \
-          VALUES (?, ?)", my_type_arr_2);
-    sqconn.commit()
-    #fname = "db/stocks." + dbname + "." + "%s" % (today_ordinal) + ".csv"
-    #print fname
-    #writer = csv.writer(open(fname, 'wb', buffering=0))
-    #return writer
+list2 = [
+	["MENT", 20.0],
+	["MSFT", 30.0],
+	["CSCO", 40.0],
+	["XICO", 50.0],
+	["PSFT", 60.0]
+]
+write_daily_values('STOCK_PRICES',list2);
+sqconn = sqlite3.connect("mystk.db");
+cursor = sqconn.execute("SELECT id, symbol, val  from STOCK_PRICES")
+for row in cursor:
+   print "ID = ", row[0]
+   print "SYMBOL = ", row[1]
+   print "VAL = ", row[2], "\n"
 
-def write_history_day (dbname, data):
-    sqconn = sqlite3.connect("mystk.db");
-    sqconn.execute("UPDATE DATETABLE SET TODAY = ? WHERE ID=1", today_ordinal);
-    my_type_arr_2 = tuple([tuple([row]) for row in data]);
-    sqconn.executemany("INSERT INTO dbname (SYMBOL,VAL) \
-          VALUES (?, ?)", my_type_arr_2);
-    sqconn.commit()
-
-def open_reader(dataname):
-    stocks = [];
-    sqconn = sqlite3.connect("mystk.db");
-    cursor = sqconn.execute("SELECT DISTINCT symbol from STOCK_PRICES")
-    for row in cursor:
-	rowVal = [];
-	symbolName = row[0];
-	rowVal.append(symbolName);
-	symData = sqconn.execute("SELECT VAL from ? where SYMBOL=?",dataname,symbolName);
-	for val in symData:
-	    rowVal.append(val);
-	stocks.append(rowVal);
-   
-    sqconn.close()
-    return stocks; 
-
-def check_update(dbname):
-    today = date.today()
-    dbFound = 0
-    today_ordinal = date.toordinal(today)
-    sqconn = sqlite3.connect("mystk.db");
-    cursor = sqconn.execute("SELECT TODAY from DateTable")
-    for row in cursor:
-	stored_date = row[0];
-    if (stored_date == today_ordinal):
-	dbFound = 1
-
-    sqconn.close()
-    return dbFound
-
+myvals = open_reader('STOCK_PRICES');
+for abc in myvals:
+	print abc[0];
 print "Connected succesffuly"
